e1174d12ff045286436357de5013b128cbb2b463
Robson Ferreira <tech.rob@icloud.com>
2022-01-19 16:13:02 -0500

    updates

--- employees_db/employees.py
+++ employees_db/employees.py
@@ -113,8 +113,88 @@ class Employees:
         except Error as e:
             print(e)
 
+    def employees_gender(self):
+        "this function will display the employees gender as Male and Female"
+        try:
+            conn = self.make_connection()
+            cursor = conn.cursor()
+            cursor.execute(
+                '''
+                SELECT 
+                    emp_no, first_name, last_name,
+                CASE
+                    WHEN gender = 'M' THEN 'Male'
+                ELSE 'Female'
+                END AS gender
+                FROM employees LIMIT 20;
+                '''
+            )
+            [print(row) for row in cursor]
+            cursor.close()
+            conn.close()
+        except Error as e:
+            print(e)
 
-employees = Employees('root', '', 'localhost', 'employees')
+    def get_salary_difference(self, value1=30000, value2=20000):
+        '''This function will display the difference between the highest and lowest salaries'''
+        try:
+            conn = self.make_connection()
+            cursor = conn.cursor()
+            cursor.execute(
+            '''
+            SELECT e.emp_no, e.first_name, e.last_name,
+            MAX(s.salary) - MIN(s.salary) AS salary_difference,
+            CASE
+                WHEN MAX(s.salary) - MIN(s.salary) > %s THEN
+                'Salary was raised by more than %s'
+                WHEN MAX(s.salary) - MIN(s.salary) 
+                BETWEEN %s AND %s THEN
+                'Salary was raised by more than %s but less than %s'
+                ELSE 'Salary was raised by less than %s'
+            END AS Salary_increase
+            FROM dept_manager dm 
+            JOIN employees e
+            ON e.emp_no = dm.emp_no
+            JOIN salaries s
+            ON s.emp_no = dm.emp_no
+            GROUP BY S.emp_no;
+            ''', (value1, value1, value2, value1, value2, value1, value2))
+            [print(row) for row in cursor]
+            cursor.close()
+            conn.close()
+        except Error as e:
+            print(e)
+
+    def is_current_employee(self):
+        "This function will display employees and if they are still with the company or not"
+        try:
+            conn = self.make_connection()
+            cursor = conn.cursor()
+            cursor.execute(
+                '''
+                SELECT
+                    e.emp_no, e.first_name, e.last_name,
+                CASE
+                    WHEN MAX(de.to_date) > sysdate()
+                    THEN 'Still with the company'
+                    ELSE 'Not with the company'
+                    END AS is_current_employee
+                FROM employees e
+                JOIN dept_emp de
+                ON e.emp_no = de.emp_no
+                GROUP BY e.emp_no
+                LIMIT 20;
+                '''
+            )
+            [print(row) for row in cursor]
+            cursor.close()
+            conn.close()
+        except Error as e:
+            print(e)
+    
+
+
+employees = Employees('root', input('enter mysql password: '), 'localhost', 'employees')
 employees.make_connection()
 employees.show_database()
 employees.show_all_tables() 
@@ -123,6 +203,9 @@ employees.show_all_tables()
 # employees.show_table_content('salaries')
 # employees.show_employees_with_parameter('first_name = "John"')
 # employees.show_employees_with_parameter('hire_date = "2000-01-01"')
-employees.employees_with_same_salary(80000)
-employees.employees_salary_higher_than_120000(120000)
+# employees.employees_with_same_salary(80000)
+# employees.employees_salary_higher_than_120000(120000)
+# employees.employees_gender()
+# employees.get_salary_difference(20000)
+employees.is_current_employee()
 
